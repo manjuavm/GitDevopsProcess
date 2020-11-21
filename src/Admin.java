cf8dae6bc7d69cd89ad9e0ef0842e3c948828ea8
manjuavm <manjuavm@gmail.com>
2020-11-21 19:23:43 +0530

    Changed the Admin page message

--- src/Admin.java
+++ src/Admin.java
@@ -4,7 +4,7 @@ public class Admin {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-			System.out.println("Welocme to Admin Page");
+			System.out.println("Welocme to Admin Page user xyz");
 	}
 
 }
